a5d70c2786ace1f6de1d5fa7981d91a5ddff8ebc
mrinex <mrinex@mail.ru>
2019-09-30 11:00:12 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,71 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+
+    let exprNoSpace = expr.replace(/\s/g,'');
+    let regex = /[0-9]+|\W/g;
+    let regexNum = /^-\d*\.?\d+$|[0-9]+/;
+    let left = 0; let right = 0;
+    let a = "TypeError: Division by zero.";
+    let b = "ExpressionError: Brackets must be paired";
+    let array = exprNoSpace.match(regex);
+    for(let i = 0; i < array.length; i++)
+    {
+      if(array[i] === '('){left += 1;}
+      if(array[i] === ')'){right += 1;}
+    }
+    if(left !== right){throw new TypeError(b);}
+  while(array.length > 1) {
+    for( i = 0; i < array.length; i++)
+    {
+      if(/\(/.test(array[i-1]) && regexNum.test(array[i]) && /\)/.test(array[i+1]))
+      {
+        array.splice(i-1, 1);array.splice(i, 1);
+        i = 0;
+      }
+      if(regexNum.test(array[i-1]) && /\*/.test(array[i]) && regexNum.test(array[i+1]) && array[i+3] !== '(' && array[i-3] !== ')')
+      {
+        let answer = Number(array[i-1]) * Number(array[i+1]);
+        array[i] = answer;//array[i-1] = ''; array[i+1] = '';
+        array.splice(i-1, 1);array.splice(i, 1);
+        i = 0;
+      }
+      if(regexNum.test(array[i-1]) && /\//.test(array[i]) && regexNum.test(array[i+1]) && array[i+3] !== '(' && array[i-3] !== ')')
+      {
+        if(Number(array[i+1]) === 0) 
+          {throw new TypeError(a);}
+        let answer = Number(array[i-1]) / Number(array[i+1]);
+		    array[i] = answer;//array[i-1] = ''; array[i+1] = '';
+        array.splice(i-1, 1);array.splice(i, 1);
+        i = 0;
+      }
+    //}
+	  //for( i = 1; i < array.length; i++)
+    //{
+	    if(regexNum.test(array[i-1]) && /\+/.test(array[i]) && regexNum.test(array[i+1]) && array[i+2] !== '*' && array[i+2] !== '/' && array[i-2] !== '*' && array[i-2] !== '/' && array[i+3] !== '(' && array[i-3] !== ')')
+      {
+        let answer = Number(array[i-1]) + Number(array[i+1]);
+		    array[i] = answer;//array[i-1] = ''; array[i+1] = '';
+        array.splice(i-1, 1);array.splice(i, 1);
+        i = 0;
+      }
+      if(regexNum.test(array[i-1]) && /\-/.test(array[i]) && regexNum.test(array[i+1]) && array[i+2] !== '*' && array[i+2] !== '/' && array[i-2] !== '*' && array[i-2] !== '/' && array[i+3] !== '(' && array[i-3] !== ')')
+      {
+        let answer = Number(array[i-1]) - Number(array[i+1]);
+		    array[i] = answer;//array[i-1] = ''; array[i+1] = '';
+        array.splice(i-1, 1);array.splice(i, 1);
+        i = 0;
+      } 
+    }
+    //for( i = 1; i < array.length; i++)
+    //{
+    //  if(/\(/.test(array[i-1]) && regexNum.test(array[i]) && /\)/.test(array[i+1]))
+    //  {
+    //  	array.splice(i-1, 1);array.splice(i, 1);
+    //  }
+    //}
+  }
+  return array = Number(array);
 }
 
 module.exports = {
